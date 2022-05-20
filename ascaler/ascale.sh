57c1c111e2092b1158c3d9d33c5c3c30d9bdf19d
Unclephil <koenigphil@gmail.com>
2022-05-20 10:11:02 +0200

    version 1.00

--- ascaler/ascale.sh
+++ ascaler/ascale.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # external variables
 LOOP=${LOOP:='yes'}
-CPU UPPER_LIMIT=${CPU_UPPER_LIMIT:=85}
+CPU_UPPER_LIMIT=${CPU_UPPER_LIMIT:=85}
 CPU_LOWER_LIMIT=${CPU_LOWER_LIMIT:=25}
 PROMETHEUS_URL=${PROMETHEUS_URL:=http://apps6.local:9090}
+##wait time WITH units 3600s 60m 1h 
+WAIT_TIME=${WAIT_TIME:=60s}
 
 # internal variables
 
@@ -12,8 +14,8 @@ PROMETHEUS_API="api/v1/query?query="
 ## PKO acquire all service with the label "swarm.autoscaler=true"
 ## return the service name, the min replica, the max replica 
 ## ================================================================
-get_svc_autoscaler() {
-    local services=$(docker service inspect $(docker service ls -q) | jq  '[.[] | select(.Spec.Labels["ascaler"]=="true")| {name:.Spec.Name, min:.Spec.Labels["ascaler.minimum"], max:.Spec.Labels["ascaler.maximum"],repl:.Spec.Mode.Replicated.Replicas}] ')
+get_svc_ascaler() {
+    local services=$(docker service inspect $(docker service ls -q) | jq  '[.[] | select(.Spec.Labels["ascaler"]=="true")| [ .Spec.Name, .Spec.Labels["ascaler.minimum"], .Spec.Labels["ascaler.maximum"], .Spec.Mode.Replicated.Replicas] ] ')
     echo $services 
 }
 
@@ -23,7 +25,7 @@ get_svc_autoscaler() {
 get_svc_metrics() {
   local SVC=$1
   local PQUERY=sum%28rate%28container_cpu_usage_seconds_total%7Bcontainer_label_com_docker_swarm_service_name%3D%22$SVC%22%7D%5B5m%5D%29%20%29%2A100
-  local value=$(curl --silent "${PROMETHEUS_URL}/${PROMETHEUS_API}${PQUERY}" | jq '.data.result[0].value[1]')
+  local value=$(curl --silent "${PROMETHEUS_URL}/${PROMETHEUS_API}${PQUERY}" | jq -r '.data.result[0].value[1]')
   echo $value 
 }
 
@@ -31,41 +33,71 @@ get_svc_metrics() {
 ## param 1 : services
 ## param 2 : new repl value
 scale() {
-  docker service scale $1=$2
-  echo "Service $1 scaled to $2 replicas"
+  docker service scale $1=$2 >>/dev/null
+  echo "INFO:  $1 scaled to $2 replicas"
 } 
 
 
 check () {
     # get services 
-    assvcs= $(get_svc_autoscaler)
+    assvcs=$(get_svc_ascaler)
+    echo $assvcs
 
+echo "debug"
+items=$(echo "$assvcs" | jq -c -r '.[]')
+for item in ${items[@]}; do
+    ## split data
+    local  svc=$(echo "$item" | jq -c -r '.[0]')
+    local  min=$(echo "$item" | jq -c -r '.[1]')
+    local  max=$(echo "$item" | jq -c -r '.[2]')
+    local repl=$(echo "$item" | jq -c -r '.[3]')
+    echo check $svc
 
-    # loop in labeled services
-    for assvc in assvcs ; do
-      echo "test  $assvc "
-      assvc_name=
-      assvc_min=
-      assvc_max=
-      assvc_repl=
-      ## set default replicas (min or max)
-      ## if repl < min =>> set repl to min
+    ## set default replicas (min or max)
+    ## if repl < min =>> set repl to min
+    if [ $repl \< $min ]; then
+      scale $svc $min 
+    elif [ $repl \> $max ]; then
+      scale $svc $max
+    else
+      echo 
+    fi
 
-      ## if repl > max =>> set repl to max
-
-      ## get metrics 
-      metric = $(get_svc_metrics $assvc)
-      ## if metric  > CPU_UPPER_LIMIT =>> scale +1
-
-      ## if metric  < CPU_LOWER_LIMIT =>> scale -1
-    done
+    ## get metrics 
+    metric=$(get_svc_metrics $svc)
+    echo $svc load is $metric %
+    if [ $metric \< $CPU_LOWER_LIMIT ]; then
+      newrepl=$(( repl-1 ))
+      if [ "$newrepl" -ge "$min" ]; then
+        scale $svc $newrepl
+      fi
+    elif [ $metric \> $CPU_UPPER_LIMIT ]; then
+      newrepl=$(( repl+1 ))
+      if [ "$newrepl" -le "$max" ]; then
+        scale $svc $newrepl
+      else
+        echo "WARNING: $svc need more replica than authorized" 
+      fi
+    else
+      echo 
+    fi
+  done
 }
 
 ## Main loop
+echo "****************************************************************************"
+echo "**  ASCALER V1.00                                                         **"
+echo "**  A Docker Swarm Simple Scaler                                          **"
+echo "**                                                                        **"
+echo "**  Base Idea : docker-swarm-autoscaler                                   **"
+echo "**  Rewrite by Unclephil                                                  **"
+echo "**  sources: https://github.com/UnclePhil/ascaler                         **"
+echo "**                                                                        **"
+echo "****************************************************************************"
 
 check
 while [[ $LOOP == 'yes' ]]; do
-  echo "Waiting 60 seconds for the next test"
-  sleep 60s
+  echo "Waiting $WAIT_TIME"
+  sleep $WAIT_TIME
   check
 done
